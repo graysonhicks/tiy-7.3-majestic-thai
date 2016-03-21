e1bbb873e162cce22eecf31582a803885d25ccf1
Grayson Hicks <graysonhicks@gmail.com>
2016-03-21 09:28:39 -0400

    fixed google map

--- app/scripts/components/location.jsx
+++ app/scripts/components/location.jsx
@@ -1,9 +1,41 @@
 var $ = require('jquery');
 var React = require('react');
 var ReactDOM = require('react-dom');
+var update = require("react-addons-update");
 var _ = require('underscore');
 var Backbone = require('backbone');
 require('backbone-react-component');
+var google = require('react-google-maps');
+var ScriptjsLoader = require("react-google-maps/lib/async/ScriptjsLoader");
+var GoogleMap = google.GoogleMap;
+var Marker = google.Marker;
+var GoogleMapLoader = google.GoogleMapLoader;
+
+var SimpleMap = React.createClass({
+  render: function(){
+    return (
+      <section style={{height: "100%"}}>
+      <GoogleMapLoader
+        containerElement={
+          <div
+            {...this.props}
+            style={{
+              height: "100%",
+            }}
+          />
+        }
+        googleMapElement={
+          <GoogleMap
+            defaultZoom={16}
+            defaultCenter={{lat: 34.851636, lng: -82.39862}}
+            >
+          </GoogleMap>
+        }
+      />
+    </section>
+    );
+  }
+});
 
 
 var LocationComponent = React.createClass({
@@ -21,9 +53,8 @@ var LocationComponent = React.createClass({
             <button type="button" className="close" onClick={this.handleReturn} data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             <h4 className="modal-title">Our Location</h4>
           </div>
-          <div className="modal-body">
-              <h4>Sorry!</h4>
-            <p>Our map is currently under construction!</p>m
+          <div className="modal-body location-modal-body">
+            <SimpleMap />
           </div>
           <div className="modal-footer">
             <button type="button" className="btn btn-default location-close-btn" onClick={this.handleReturn} data-dismiss="modal">Close</button>
