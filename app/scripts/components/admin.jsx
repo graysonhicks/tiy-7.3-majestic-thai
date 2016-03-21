c17f4f1276b5dbba97972630dcbc97e83af0a47a
Grayson Hicks <graysonhicks@gmail.com>
2016-03-21 12:50:07 -0400

    fixed logout button on admin

--- app/scripts/components/admin.jsx
+++ app/scripts/components/admin.jsx
@@ -8,14 +8,15 @@ var parsley = require('parsleyjs');
 
 var OpenOrdersComponent = React.createClass({
   mixins: [Backbone.React.Component.mixin],
-  handleClick: function(item){
-
+  handleClick: function(orderId){
+    $(this).addClass("active");
+    this.props.setCurrentOrder(orderId);
   },
   render: function(){
       var newItem = function(item){
-        console.log(item);
+        var orderId = item.cid;
         return (
-        <li className="list-group-item open-orders-items" key={item.get('_id')}>
+        <li className="list-group-item open-orders-items" onClick={this.handleClick.bind(this, orderId)} key={item.cid}>
           <div className="row open-orders-row">
             <div className="col-md-12 cart-item-dish">
               <h4>{item.attributes.customer.name}</h4>
@@ -38,17 +39,45 @@ var OpenOrdersComponent = React.createClass({
 var OrderDetailsComponent = React.createClass({
   mixins: [Backbone.React.Component.mixin],
   render: function(){
-        return (
-          <h1>Detail Orders</h1>
+    var mapOrders = function(item){
+        var mapDishes = function(order){
+          return (
+            <li className="list-group-item order-details-dishes">
+              {order.dish}
+            </li>
           )
         }
-      });
+        return(
+          <div>
+            <h2 className="order-details-name"><span className="order-details-for">for: </span>{item.attributes.customer.name}</h2>
+            <ul className="list-group">
+              {(item.attributes.orders.map(mapDishes.bind(this)))}
+            </ul>
+            <h4>Total Due: ${item.attributes.totalPrice}</h4>
+            <button type="button" className="btn btn-success btn-lg order-details-complete-btn btn-block">Complete!</button>
+          </div>
+        )
+      }
+    var filterOrders = function(item){
+      return item.cid == this.props.currentOrder
+    }
+    return (
+      <div className="detailed-order">
+        {(this.props.collection.filter(filterOrders.bind(this)).map(mapOrders.bind(this)))}
+      </div>
+    )
+  }
+});
 
 var AdminComponent = React.createClass({
   mixins: [Backbone.React.Component.mixin],
+  componentWillMount: function(){
+    this.props.orderCollection.fetch();
+  },
   getInitialState: function(){
     return{
       orders: this.props.orderCollection,
+      currentOrder: this.props.orderCollection.first.cid
     }
   },
   removeItem: function(item){
@@ -57,12 +86,14 @@ var AdminComponent = React.createClass({
     //   orders: this.props.orderCollection
     // });
   },
+  setCurrentOrder: function(orderId){
+    this.setState({"currentOrder": orderId});
+  },
   handleReturn: function(e){
     e.preventDefault();
     Backbone.history.navigate("", {trigger: true});
   },
   render: function(){
-    console.log(this.state.orders.fetch());
         return (
               <div className="modal fade admin-login-modal-container" id="adminModal" tabIndex="-1" role="dialog">
                 <div className="modal-dialog admin-modal-dialog modal-lg">
@@ -71,20 +102,21 @@ var AdminComponent = React.createClass({
                       <button type="button" className="close" onClick={this.handleReturn} data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                       <h4 className="modal-title">Sukho Thai Open Orders</h4>
                     </div>
-                    <div className="modal-body">
+                    <div className="modal-body admin-modal-body">
                       <div className="row">
                         <div className="col-md-3 open-orders-column">
                           <h2 className="admin-headings">Open Orders</h2>
-                          <OpenOrdersComponent collection={this.state.orders} />
+                          <OpenOrdersComponent setCurrentOrder={this.setCurrentOrder} collection={this.state.orders} />
                         </div>
                         <div className="col-md-9">
                           <h2 className="admin-headings">Order Details</h2>
-                          <OrderDetailsComponent collection={this.state.orders} />
+                          <OrderDetailsComponent currentOrder={this.state.currentOrder} collection={this.state.orders} />
                         </div>
                       </div>
                     </div>
                     <div className="modal-footer">
                       <button type="button"
+                        onClick={this.handleReturn} 
                          id="admin-logout"
                         className="btn btn-danger admin-logout-btn"
                         >Logout</button>
---
+++ cookies.js
@@ -0,0 +1,3 @@
+document.cookie="username=Grayson Hicks";
+document.cookie="likes-cookies=yes";
+console.log($.cookie('username'));
